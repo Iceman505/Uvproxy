b6d79f0e5c0b0dce5bc6d2d3809149c6655e69e6
Duffman3 <151552261+Duffman3@users.noreply.github.com>
2024-01-09 13:27:13 -0600

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://phantomnetwork.com/bare/',
+    bare: 'https://artclass.site/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
